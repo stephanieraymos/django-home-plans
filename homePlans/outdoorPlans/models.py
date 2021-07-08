94629df4af09d390549d5d4c7b4c0f21cefbac35
Stephanie Raymos <stephanieraymos@gmail.com>
2021-07-08 11:30:07 -0700

    Summary property in Outdoor Plans

--- homePlans/outdoorPlans/models.py
+++ homePlans/outdoorPlans/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 
 class OutdoorPlan(models.Model):
-    # Images
     image = models.ImageField(upload_to='images/')
-    # Summary
+    summary = models.CharField(max_length=200)
